8ff221a90e33bcf3dbc828161247877e6a272d3d
gbc <gbc7@cin.ufpe.br>
2020-02-07 13:26:22 -0300

    SparseTable class

--- code/DataStructures/SparseTable.cpp
+++ code/DataStructures/SparseTable.cpp
@@ -3,27 +3,32 @@
 
 using namespace std;
 
-vector<vector<int>> table;
-vector<int> lg, v;
-int n;
-
-void build(){
-	lg.resize(n+1);
-	lg[1] = 0;
-	for(int i = 2; i <= n; ++i) lg[i] = lg[i/2] + 1;
-	table.resize(n+1, vector<int>(lg[n]+1));
-	for(int i = 0; i < n; ++i) table[i][0] = v[i];
-	for(int j = 0; j < lg[n]; ++j){
-		for(int i = 0; i + (1 << j) < n; ++i){
-			table[i][j+1] = min(table[i][j], table[i+(1 << j)][j]);
+template<class T>
+class SparseTable{
+	private:
+		vector<vector<T>> table;
+		vector<int> lg;
+	public:
+		void build(vector<T> v){
+			int n = v.size();
+			lg.resize(n+1);
+			lg[1] = 0;
+			for(int i = 2; i <= n; ++i) lg[i] = lg[i/2] + 1;
+			table.resize(n+1, vector<T>(lg[n]+1));
+			for(int i = 0; i < n; ++i) table[i][0] = v[i];
+			for(int j = 0; j < lg[n]; ++j){
+				for(int i = 0; i + (1 << j) < n; ++i){
+					table[i][j+1] = max(table[i][j], table[i+(1 << j)][j]);
+				}
+			}
 		}
-	}
-}
+		T query(int l, int r){
+			if(l > r) return inf;
+			int j = lg[r-l+1];
+			return max(table[l][j], table[r - (1 << j) + 1][j]);
+		}
+};
 
-int query(int l, int r){
-	int j = lg[r-l+1];
-	return min(table[l][j], table[r - (1 << j) + 1][j]);
-}
 
 int main(){
 	ios::sync_with_stdio(0);
