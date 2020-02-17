e9a4ae7420c8af8a45aa498b0f9c9b295eac3a50
gbc <gbc7@cin.ufpe.br>
2020-02-17 18:24:42 -0300

    added LCA

--- code/Graphs/LCA.cpp
+++ code/Graphs/LCA.cpp
@@ -2,72 +2,33 @@
 
 using namespace std;
 
-class LCA{
-	private:
-		vector<int> height, euler, first, tree;
-		vector<bool> vs;
-		int n;
-		
-		void dfs(vector<vector<int>> &g, int u, int h = 0){
-			vs[u] = true;
-			height[u] = h;
-			first[u] = euler.size();
-			euler.push_back(u);
-			for(auto v : g[u]){
-				if(!vs[v]){
-					dfs(g, v, h+1);
-					euler.push_back(u);
-				}
-			}
-		}
-		
-		void build(int pos, int i, int j){
-			if(i == j) tree[pos] = euler[i];
-			else{
-				int esq = 2*pos;
-				int dir = 2*pos + 1;
-				int mid = (i+j)/2;
-				build(esq, i, mid);
-				build(dir, mid+1, j);
-				int l = tree[esq], r = tree[dir];
-				tree[pos] = (height[l] < height[r])? l : r;
-			}
-		}
-		
-		int query(int pos, int i, int j, int l, int r){
-			int esq = 2*pos;
-			int dir = 2*pos + 1;
-			int mid = (i+j)/2;
-			if(i > r || j < l) return -1;
-			else if(i >= l && j <= r) return tree[pos];
-			else{
-				int left = query(esq, i, mid, l, r);
-				int right = query(dir, mid+1, j, l, r);
-				if(left == -1) return right;
-				if(right == -1) return left;
-				return (height[left] < height[right])? left : right;
-			}
-		}
-	
-		
-	public:
-		LCA(vector<vector<int>> &g, int root = 0){
-			n = g.size();
-			height.resize(n);
-			first.resize(n);
-			euler.reserve(2*n);
-			vs.assign(n,0);
-			dfs(g,root);
-			tree.resize(euler.size()*4);
-			build(1,0,euler.size()-1);
-		}
-		
-		int qry(int u, int v){
-			int l = first[u], r = first[v];
-			if(l > r) swap(l,r);
-			return query(1,0,euler.size()-1,l,r);
-		}
-};
+int t,l,n;
+vector<int> tin, tout;
+vector<vector<int>> up;
+vector<vector<int>> g;
+
+void dfs(int u, int p){
+	tin[u] = ++t;
+	up[u][0] = p;
+	for(int i = 1; i <= l; ++i) up[u][i] = up[up[u][i-1]][i-1];
+	for(int v : g[u]){
+		if(v != p) dfs(v,u);
+	}
+	tout[u] = ++t;
+}
+
+bool ancestor(int u, int v){
+	return (tin[u] <= tin[v] && tout[u] >= tout[v]);
+}
+
+int lca(int u, int v){
+	if(ancestor(u,v)) return u;
+	if(ancestor(v,u)) return v;
+	for(int i = l; i >= 0; --i){
+		if(!ancestor(up[u][i],v)) u = up[u][i];
+	}
+	return up[u][0];
+}
 
 int main(){
 	ios::sync_with_stdio(0);
