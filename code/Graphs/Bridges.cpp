956c715d6ce534ceba03be22b1940f9eb5846734
Gabriel <gbc7@cin.ufpe.br>
2020-11-09 20:16:21 -0300

    added Bridges

---
+++ code/Graphs/Bridges.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<vector<int>> g;
+vector<int> vs,tin,low;
+vector<pii> bridges;
+int t = 1;
+
+void dfs(int v, int p){
+	vs[v] = 1;
+	tin[v] = low[v] = t++;
+	for(int u : g[v]){
+		if(!vs[u]){
+			dfs(u,v);
+			low[v] = min(low[v],low[u]);
+			if(low[u] > tin[v]) bridges.pb({v,u});
+		}else if(u != p){
+			low[v] = min(low[v],tin[u]);
+		}
+	}
+}
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	return 0;
+}
